859b0afb5c027525a59c2077fa5f99e03dfb1538
Peterson Zeferino <petersonzeferino@hotmail.com>
2020-06-07 01:04:16 -0300

    Write and Read CSV

---
+++ Component/CreateCSV.cs
@@ -0,0 +1,33 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ReadAndWriteFiles.Component
+{
+    public static class CreateCSV<T>
+    {
+        public static void WriteCSV(IList<T> records, string path = "")
+        {
+            using (var writer = new StreamWriter(path))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.Configuration.Delimiter = ";";
+                csv.WriteRecords(records);
+            }
+        }
+
+        public static IEnumerable<T> ReadCSV(string path)
+        {
+            using (var reader = new StreamReader(path))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                return csv.GetRecords<T>().ToArray();
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,64 @@
-﻿using System;
+﻿using ReadAndWriteFiles.Component;
+using ReadAndWriteFiles.Models;
+using System;
+using System.Collections.Generic;
 
 namespace ReadAndWriteFiles
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+        }
+
+        private static void CreateCSV()
+        {
+            CreateCSV<FileModel>.WriteCSV(FactoryFileModel());
+        }
+
+        private static IEnumerable<FileModel> ReadCSV(string path)
+        {
+            IEnumerable<FileModel> file = CreateCSV<FileModel>.ReadCSV(path);
+            return file;
+        }
+
+        private static List<FileModel> FactoryFileModel()
+        {
+            List<FileModel> list = new List<FileModel>();
+
+            FileModel fileModelOne = new FileModel()
+            {
+                Nome = "Test One",
+                Cpf = 12345678900,
+                Telefone = "11234567",
+                Celular = "912345678",
+                Data = DateTime.Today.ToString()
+            };
+
+            FileModel fileModelTwo = new FileModel()
+            {
+                Nome = "Test Two",
+                Cpf = 22345678900,
+                Telefone = "21234567",
+                Celular = "922345678",
+                Data = DateTime.Today.ToString()
+            };
+
+            FileModel fileModelThree = new FileModel()
+            {
+                Nome = "Test Three",
+                Cpf = 32345678900,
+                Telefone = "31234567",
+                Celular = "932345678",
+                Data = DateTime.Today.ToString()
+            };
+
+            list.Add(fileModelOne);
+            list.Add(fileModelTwo);
+            list.Add(fileModelThree);
+
+            return list;
         }
     }
 }
