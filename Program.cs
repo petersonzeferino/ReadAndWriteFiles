af23907d64862770b9dfabd9cdab3e52a9276d49
Peterson Zeferino <petersonzeferino@hotmail.com>
2020-06-07 00:21:15 -0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReadAndWriteFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
