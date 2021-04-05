79f108709a61bdbdf63e51cf4978684e02f5ffc7
Peterson <petersonzeferino@hotmail.com>
2021-04-05 17:08:03 -0300

    Create Project Test

---
+++ ReadAndWriteFilesTest/Fixtures/TestsFixture.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReadAndWriteFilesTest.Fixtures
+{
+    public class TestsFixture : IDisposable
+    {
+        public TestsFixture()
+        {
+            // Do "global" initialization here; Only called once.
+        }
+
+        public void Dispose()
+        {
+            // Do "global" teardown here; Only called once.
+        }
+    }
+}
