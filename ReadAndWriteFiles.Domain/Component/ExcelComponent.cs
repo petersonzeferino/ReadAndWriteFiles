742265e36f6618799a4c78e718a137617b954029
Peterson <petersonzeferino@hotmail.com>
2021-09-13 22:28:10 -0300

    Install EEplus

--- ReadAndWriteFiles.Domain/Component/ExcelComponent.cs
+++ ReadAndWriteFiles.Domain/Component/ExcelComponent.cs
@@ -1,4 +1,4 @@
-﻿using ClosedXML.Excel;
+﻿using OfficeOpenXml;
 using ReadAndWriteFiles.Domain.Entities.CSV;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,51 +7,25 @@ namespace ReadAndWriteFiles.Domain.Component
 {
     public static class ExcelComponent
     {
-        //public static void WriteExcel(IList<RequestEntity> records, string path = "")
-        //{
-        //    using (var workbook = new XLWorkbook())
-        //    {
-        //        var worksheet = workbook.Worksheets.Add("Person");
-        //        worksheet.Cell(1, 1).Value = "Nome";
-        //        worksheet.Cell(1, 2).Value = "Cpf";
-        //        worksheet.Cell(1, 3).Value = "Telefone";
-        //        worksheet.Cell(1, 4).Value = "Celular";
-        //        worksheet.Cell(1, 5).Value = "Data";
+        public static ExcelWorksheet RemoverLinhasVazias(ExcelWorksheet worksheet)
+        {
+            while (VerificarUltimaLinha(worksheet))
+                worksheet.DeleteRow(worksheet.Dimension.End.Row);
 
-        //        for (int index = 1; index <= records.Count; index++)
-        //        {
-        //            worksheet.Cell(index + 1, 1).Value = records[index - 1].Nome;
-        //            worksheet.Cell(index + 1, 2).Value = records[index - 1].Cpf.ToString();
-        //            worksheet.Cell(index + 1, 3).Value = records[index - 1].Telefone;
-        //            worksheet.Cell(index + 1, 4).Value = records[index - 1].Celular;
-        //            worksheet.Cell(index + 1, 5).Value = records[index - 1].Data;
-        //        }
-        //        workbook.SaveAs(path);
-        //    }
-        //}
+            return worksheet;
+        }
 
-        //ExcelWorksheet worksheet = package.Workbook.Worksheets[1];
-        //ExcelWorksheet worksheet2 = RemoverLinhasVazias(worksheet);
+        public static bool VerificarUltimaLinha(ExcelWorksheet worksheet)
+        {
+            var empties = new List<bool>();
 
-        //public ExcelWorksheet RemoverLinhasVazias(ExcelWorksheet worksheet)
-        //{
-        //    while (VerificarUltimaLinha(worksheet))
-        //        worksheet.DeleteRow(worksheet.Dimension.End.Row);
+            for (int i = 1; i <= worksheet.Dimension.End.Column; i++)
+            {
+                var rowEmpty = worksheet.Cells[worksheet.Dimension.End.Row, i].Value == null ? true : false;
+                empties.Add(rowEmpty);
+            }
 
-        //    return worksheet;
-        //}
-
-        //public static bool VerificarUltimaLinha(ExcelWorksheet worksheet)
-        //{
-        //    var empties = new List<bool>();
-
-        //    for (int i = 1; i <= worksheet.Dimension.End.Column; i++)
-        //    {
-        //        var rowEmpty = worksheet.Cells[worksheet.Dimension.End.Row, i].Value == null ? true : false;
-        //        empties.Add(rowEmpty);
-        //    }
-
-        //    return empties.All(e => e);
-        //}
+            return empties.All(e => e);
+        }
     }
 }
